634566422f80fe0a05972fc0958461d0145d197e
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-01-23 16:48:11 +0100

    Update authController.jsaaaaaaa

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,23 +44,31 @@ const user = response;
 
 
      res.cookie("access_token", access_token, {
-httpOnly: true, domain: '.onrender.com'
+httpOnly: true,
+  domain: '.onrender.com',
+  secure: true,
 
 });
 
 res.cookie("loggedIn", "loggedIn", {
- httpOnly: true, domain: '.onrender.com'
+httpOnly: true,
+  domain: '.onrender.com',
+  secure: true,
 
 });
 
 res.cookie("idCRM", user.idCRM, {
- httpOnly: true, domain: '.onrender.com'
+httpOnly: true,
+  domain: '.onrender.com',
+  secure: true,
  
 });
 
 res.cookie("idUser", user._id.toString(), {
   
-httpOnly: true, domain: '.onrender.com'
+httpOnly: true,
+  domain: '.onrender.com',
+  secure: true,
 
 });
 

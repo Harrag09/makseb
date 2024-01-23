0df51b1ec9fc9f8c466cff5ebb90ca7c4d1ff3ba
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-01-23 13:15:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ const authRoutes = require('./routes/auth.js');
 const usersRoutes = require('./routes/users.js');
 const cors = require("cors");
 
-
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// Enable CORS
+app.use(cors({
+    origin: ['https://harrag09.github.io/maksebfrontend', 'http://localhost:3000'],
+    credentials: true,
+}));
+
+app.options('*', cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // Connect to MongoDB
 connectToDatabase();
@@ -20,19 +32,13 @@ connectToDatabase();
 // Routes
 app.use('/', livestatsRoutes);
 app.use('/', authRoutes);
-app.use('/api',usersRoutes)
-
-app.use(cors({
-    origin: ['https://harrag09.github.io/maksebfrontend',"http://localhost:3000"],
-    credentials: true,
-}))
-
+app.use('/api', usersRoutes);
 
 const PORT = 8002;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;

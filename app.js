1718df473a9f3bfcb2ff1437dac40084df245efd
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-01-24 10:07:17 +0100

    Update app.jssdqsdqs

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-    origin: '*', 
+  
+  origin: ['https://harrag09.github.io/maksebfrontend/',"http://localhost:3000","http://localhost:3001"],
     methods: 'GET,POST,DELETE,PUT',
     credentials: true,
 }));

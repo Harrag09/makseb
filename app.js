3020629fb3f77e34be50c97b27cabbe74030ef92
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-01-24 09:14:40 +0100

    Update app.jsssssssssssss

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const cors = require("cors");
 const app = express();
 
 // Middleware to set common headers for all routes
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(cors({

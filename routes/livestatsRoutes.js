413965ba8d43c7d139c6331080b2dfacbe71944b
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-02-20 11:18:20 +0100

    Update livestatsRoutes.jsaa

--- routes/livestatsRoutes.js
+++ routes/livestatsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const livestatsController = require("../controllers/livestatsController.js");
 const verifyToken = require("../utils/verifyToken.js");
 
-const { getLivestat, getLivestatById, getLivestatByIdandDate, updateLivestat, updateLivestat2, updateLivestat3,updateStatusStores  } = livestatsController;
+const { getLivestat, getLivestatById, getLivestatByIdandDate, updateLivestat, updateLivestat2, updateLivestat3,updateStatusStores ,GetLicence } = livestatsController;
 const { verifyAccessToken } = verifyToken;
 
 const livestatsRoutes = express.Router();
@@ -14,5 +14,6 @@ livestatsRoutes.post("/Update3", updateLivestat3);
 livestatsRoutes.get("/livestats2", getLivestat);
 livestatsRoutes.get("/SumData", getLivestatByIdandDate);
 livestatsRoutes.post("/statusStores", updateStatusStores);
+livestatsRoutes.get("/GetLicence/:idCRM", GetLicence);
 
 module.exports = livestatsRoutes;

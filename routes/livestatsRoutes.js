c1a6d1a49b125e4c4f5d3e2cf44d3f07625e4679
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-03-04 14:25:28 +0100

    Update livestatsRoutes.js

--- routes/livestatsRoutes.js
+++ routes/livestatsRoutes.js
@@ -2,24 +2,25 @@ const express = require("express");
 const livestatsController = require("../controllers/livestatsController.js");
 const verifyToken = require("../utils/verifyToken.js");
 
-const {getLivestatByIdandDate2,getAllCatInUploid,updateLivestat4,updateAllCatCripteInMongo,updateAllCatInUploid, getLivestat, getLivestatById, getLivestatByIdandDate, updateLivestat, updateLivestat2, updateLivestat3,updateStatusStores ,GetLicence ,UpdateLicence} = livestatsController;
+const {UpdateTiquer,getLivestatByIdandDate2,getAllCatInUploid,updateLivestat4,updateAllCatCripteInMongo,updateAllCatInUploid, getLivestatByIdandDate,   updateLivestat3,updateStatusStores ,GetLicence ,UpdateLicence} = livestatsController;
 const { verifyAccessToken } = verifyToken;
 
 const livestatsRoutes = express.Router();
 
-livestatsRoutes.get("/livestats", getLivestatById);
-livestatsRoutes.post("/Update", updateLivestat);
-livestatsRoutes.post("/Update2", updateLivestat2);
 livestatsRoutes.post("/Update3", updateLivestat3);
 livestatsRoutes.post("/Update4", updateLivestat4);
+livestatsRoutes.post("/UpdateTiquer", UpdateTiquer);
 
-livestatsRoutes.get("/livestats2", getLivestat);
 livestatsRoutes.get("/SumData", getLivestatByIdandDate);
 livestatsRoutes.get("/SumData2", getLivestatByIdandDate2);
+
 livestatsRoutes.post("/statusStores", updateStatusStores);
+
 livestatsRoutes.get("/GetLicence/:idCRM", GetLicence);
 livestatsRoutes.get("/UpdateLicence/:idCRM/:action", UpdateLicence);
+
 livestatsRoutes.post("/updateAllCatInFolder", updateAllCatInUploid);
 livestatsRoutes.post("/updateAllCatInMongobd", updateAllCatCripteInMongo);
 livestatsRoutes.get("/GetImages", getAllCatInUploid);
+
 module.exports = livestatsRoutes;

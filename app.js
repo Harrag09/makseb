667ed0b52fd5c0a4b55552532c49776b286e8a59
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-05-09 10:14:26 +0100

    Update app.jsSSSS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { connectToDatabase } = require('./config/dbConfig.js');
+const { connectToDatabase, client } = require('./config/dbConfig.js');
 const cors = require('cors');
 const livestatsRoutes = require('./routes/livestatsRoutes.js');
 const authRoutes = require('./routes/auth.js');
@@ -11,6 +11,10 @@ const app = express();
 const fs = require('fs');
 const { updateStatus } = require('./controllers/livestatsController.js');
 const CronJob = require('cron').CronJob;
+const socketIo = require('socket.io');
+const { MongoClient } = require('mongodb');
+
+
 // Enable CORS for the specific origin
 app.use(cors({
   origin: ['https://harrag09.github.io','http://localhost:3002','http://localhost:3001','http://statistics.sc3makseb.universe.wf','http://localhost:3000','http://192.168.1.2:3001','http://192.168.1.45:3001'],
@@ -57,11 +61,11 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 //CHANGE STATUS DE ALL USER CHAQUE 10 MIN
 const job = new CronJob('*/10 * * * *', updateStatus);
-
+job.start();
 //CHANGE STATUS DE ALL USER CHAQUE 5 MIN
 // const job = new CronJob('*/5 * * * *', updateStatus);
 
-job.start();
+
 
 
 
@@ -94,8 +98,29 @@ app.get('/images', (req, res) => {
 const PORT = 8002;
 
 // Start the server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+const io = socketIo(server);
+
+const db = client.db('statistiques');
+const collection = db.collection('user');
+
+const changeStream = collection.watch();
+
+changeStream.on('change', async (change) => {
+  const { documentKey ,updateDescription} = change;
+  const response = await collection.findOne({ _id:documentKey._id });
+  // console.log("Update for  :",response.Nom," with IDCRM  ",response.idCRM ," and ID ",documentKey._id ," IN :  ",updateDescription.updatedFields)
+if(updateDescription.updatedFields.Status!=undefined)
+{ io.emit(`UpdateStatusWithIDCRM${response.idCRM}`, {  _id: documentKey._id, objectUpdate: updateDescription.updatedFields,response:response }); }
+else if (updateDescription.updatedFields.Licence!=undefined)
+{ io.emit(`UpdateLicenceWithIDCRM${response.idCRM}`, {  _id: documentKey._id, objectUpdate: updateDescription.updatedFields,response:response });}
+else if (updateDescription.updatedFields.Nom!=undefined||updateDescription.updatedFields.Nom!=undefined||updateDescription.updatedFields.Nom!=undefined||updateDescription.updatedFields.Nom!=undefined)
+{ io.emit(`UpdatewithIDCRM${response.idCRM}`, {  _id: documentKey._id, objectUpdate: updateDescription.updatedFields,response:response });} 
+ else{ console.log("Update: ",response.idCRM ," and ID ",documentKey._id ," IN :  ",updateDescription.updatedFields)
+}
+console.log("Update User With IDCRM: ",response.idCRM ," and ID ",documentKey._id ," IN :  ",updateDescription.updatedFields);
 });
 
 module.exports = app;

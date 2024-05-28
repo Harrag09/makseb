6967f00989924fae030c924a3909765cf1804f9b
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-05-28 17:36:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ const PORT = 8002;
 // Start the server
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(io);
 });
 const io = socketIo(server, {
   cors: {

c25e5713f33d9509c8e7f4a84f7d2eae6223f6aa
Harrag Oussama <104853671+Harrag09@users.noreply.github.com>
2024-06-10 10:02:07 +0100

    Update livestatsRoutes.jssss

--- routes/livestatsRoutes.js
+++ routes/livestatsRoutes.js
@@ -2,15 +2,18 @@ const express = require("express");
 const livestatsController = require("../controllers/livestatsController.js");
 const verifyToken = require("../utils/verifyToken.js");
 
-const {sendWelcomeEmail,generateTicketsHTML2,generateTicketsHTML,getTiquerId,UpdateTiquer,getLivestatByIdandDate2,getAllCatInUploid,updateLivestat4,updateAllCatCripteInMongo,updateAllCatInUploid, getLivestatByIdandDate,   updateLivestat3,updateStatusStores ,GetLicence ,UpdateLicence} = livestatsController;
+const {updateLivestatForGetReglement,UpdateBaseDeDonne,GetBaseName,sendPdfInEmail,sendWelcomeEmail,generateTicketsHTML2,generateTicketsHTML,getTiquerId,UpdateTiquer,getLivestatByIdandDate2,getAllCatInUploid,updateLivestat4,updateAllCatCripteInMongo,updateAllCatInUploid, getLivestatByIdandDate,   updateLivestat3,updateStatusStores ,GetLicence ,UpdateLicence} = livestatsController;
 const { verifyAccessToken } = verifyToken;
 
 const livestatsRoutes = express.Router();
 
 livestatsRoutes.post("/Update3", updateLivestat3);
 livestatsRoutes.post("/Update4", updateLivestat4);
+livestatsRoutes.post("/UpdateGetReglement", updateLivestatForGetReglement);
 livestatsRoutes.post("/UpdateTiquer", UpdateTiquer);
 livestatsRoutes.post("/sendemail", sendWelcomeEmail);
+livestatsRoutes.post("/sendPdfInemail", sendPdfInEmail);
+
 
 
 livestatsRoutes.get("/SumData", getLivestatByIdandDate);
@@ -21,7 +24,9 @@ livestatsRoutes.get("/GetTiquer", getTiquerId);
 livestatsRoutes.post("/statusStores", updateStatusStores);
 
 livestatsRoutes.get("/GetLicence/:idCRM", GetLicence);
+livestatsRoutes.get("/GetBaseName/:idCRM", GetBaseName);
 livestatsRoutes.get("/UpdateLicence/:idCRM/:action", UpdateLicence);
+livestatsRoutes.get("/UpdateBaseDeDonne/:idCRM/:action", UpdateBaseDeDonne);
 
 livestatsRoutes.post("/updateAllCatInFolder", updateAllCatInUploid);
 livestatsRoutes.post("/updateAllCatInMongobd", updateAllCatCripteInMongo);
